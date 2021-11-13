42977ba107c46d1e29dc3a9b3a6910a7112e4aab
MustafaTugrull <75477685+MustafaTugrull@users.noreply.github.com>
2021-11-13 18:21:55 +0300

    baslangic

---
+++ TeknikServis/TeknikServis/Formlar/FrmUrunListesi.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace TeknikServis.Formlar
+{
+    partial class FrmUrunListesi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUrunListesi));
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.TxtID = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.BtnListele = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnGuncelle = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnSil = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnKaydet = new DevExpress.XtraEditors.SimpleButton();
+            this.lookUpEdit1 = new DevExpress.XtraEditors.LookUpEdit();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.TxtStok = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.TxtSatisFiyat = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.TxtAlisFiyat = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.TxtMarka = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.TxtUrunAd = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtID.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtStok.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtSatisFiyat.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtAlisFiyat.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtMarka.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtUrunAd.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(12, 3);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(1009, 550);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Appearance.Row.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(172)))), ((int)(((byte)(215)))));
+            this.gridView1.Appearance.Row.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(225)))), ((int)(((byte)(240)))));
+            this.gridView1.Appearance.Row.Options.UseBackColor = true;
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.FocusedRowChanged += new DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(this.gridView1_FocusedRowChanged);
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.TxtID);
+            this.groupControl1.Controls.Add(this.labelControl7);
+            this.groupControl1.Controls.Add(this.BtnListele);
+            this.groupControl1.Controls.Add(this.BtnGuncelle);
+            this.groupControl1.Controls.Add(this.BtnSil);
+            this.groupControl1.Controls.Add(this.BtnKaydet);
+            this.groupControl1.Controls.Add(this.lookUpEdit1);
+            this.groupControl1.Controls.Add(this.labelControl6);
+            this.groupControl1.Controls.Add(this.TxtStok);
+            this.groupControl1.Controls.Add(this.labelControl5);
+            this.groupControl1.Controls.Add(this.TxtSatisFiyat);
+            this.groupControl1.Controls.Add(this.labelControl4);
+            this.groupControl1.Controls.Add(this.TxtAlisFiyat);
+            this.groupControl1.Controls.Add(this.labelControl3);
+            this.groupControl1.Controls.Add(this.TxtMarka);
+            this.groupControl1.Controls.Add(this.labelControl2);
+            this.groupControl1.Controls.Add(this.TxtUrunAd);
+            this.groupControl1.Controls.Add(this.labelControl1);
+            this.groupControl1.Location = new System.Drawing.Point(1027, 3);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(343, 541);
+            this.groupControl1.TabIndex = 1;
+            this.groupControl1.Text = "ÜRÜN İŞLEMLERİ";
+            // 
+            // TxtID
+            // 
+            this.TxtID.Enabled = false;
+            this.TxtID.Location = new System.Drawing.Point(87, 52);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(209, 20);
+            this.TxtID.TabIndex = 22;
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Location = new System.Drawing.Point(37, 55);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(46, 13);
+            this.labelControl7.TabIndex = 21;
+            this.labelControl7.Text = "ÜRÜN ID:";
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnListele.ImageOptions.Image")));
+            this.BtnListele.Location = new System.Drawing.Point(87, 372);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(209, 35);
+            this.BtnListele.TabIndex = 20;
+            this.BtnListele.Text = "LİSTELE";
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnGuncelle.ImageOptions.Image")));
+            this.BtnGuncelle.Location = new System.Drawing.Point(87, 331);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(209, 35);
+            this.BtnGuncelle.TabIndex = 19;
+            this.BtnGuncelle.Text = "GÜNCELLE";
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnSil.ImageOptions.Image")));
+            this.BtnSil.Location = new System.Drawing.Point(87, 290);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(209, 35);
+            this.BtnSil.TabIndex = 18;
+            this.BtnSil.Text = "SİL";
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnKaydet.ImageOptions.Image")));
+            this.BtnKaydet.Location = new System.Drawing.Point(87, 249);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(209, 35);
+            this.BtnKaydet.TabIndex = 17;
+            this.BtnKaydet.Text = "KAYDET";
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // lookUpEdit1
+            // 
+            this.lookUpEdit1.EditValue = "";
+            this.lookUpEdit1.Location = new System.Drawing.Point(89, 208);
+            this.lookUpEdit1.Name = "lookUpEdit1";
+            this.lookUpEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit1.Properties.DisplayMember = "AD";
+            this.lookUpEdit1.Properties.ValueMember = "ID";
+            this.lookUpEdit1.Size = new System.Drawing.Size(207, 20);
+            this.lookUpEdit1.TabIndex = 16;
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Location = new System.Drawing.Point(28, 211);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(55, 13);
+            this.labelControl6.TabIndex = 15;
+            this.labelControl6.Text = "KATEGORİ:";
+            // 
+            // TxtStok
+            // 
+            this.TxtStok.Location = new System.Drawing.Point(87, 182);
+            this.TxtStok.Name = "TxtStok";
+            this.TxtStok.Size = new System.Drawing.Size(209, 20);
+            this.TxtStok.TabIndex = 14;
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(53, 185);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(30, 13);
+            this.labelControl5.TabIndex = 13;
+            this.labelControl5.Text = "STOK:";
+            // 
+            // TxtSatisFiyat
+            // 
+            this.TxtSatisFiyat.Location = new System.Drawing.Point(87, 156);
+            this.TxtSatisFiyat.Name = "TxtSatisFiyat";
+            this.TxtSatisFiyat.Size = new System.Drawing.Size(209, 20);
+            this.TxtSatisFiyat.TabIndex = 12;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(14, 159);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(69, 13);
+            this.labelControl4.TabIndex = 11;
+            this.labelControl4.Text = "SATIŞ FİYATI:";
+            // 
+            // TxtAlisFiyat
+            // 
+            this.TxtAlisFiyat.Location = new System.Drawing.Point(87, 130);
+            this.TxtAlisFiyat.Name = "TxtAlisFiyat";
+            this.TxtAlisFiyat.Size = new System.Drawing.Size(209, 20);
+            this.TxtAlisFiyat.TabIndex = 10;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(21, 133);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(62, 13);
+            this.labelControl3.TabIndex = 9;
+            this.labelControl3.Text = "ALIŞ FİYATI:";
+            // 
+            // TxtMarka
+            // 
+            this.TxtMarka.Location = new System.Drawing.Point(87, 104);
+            this.TxtMarka.Name = "TxtMarka";
+            this.TxtMarka.Size = new System.Drawing.Size(209, 20);
+            this.TxtMarka.TabIndex = 8;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(44, 107);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(39, 13);
+            this.labelControl2.TabIndex = 7;
+            this.labelControl2.Text = "MARKA:";
+            // 
+            // TxtUrunAd
+            // 
+            this.TxtUrunAd.Location = new System.Drawing.Point(87, 78);
+            this.TxtUrunAd.Name = "TxtUrunAd";
+            this.TxtUrunAd.Size = new System.Drawing.Size(209, 20);
+            this.TxtUrunAd.TabIndex = 6;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(30, 81);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(53, 13);
+            this.labelControl1.TabIndex = 5;
+            this.labelControl1.Text = "ÜRÜN ADI:";
+            // 
+            // FrmUrunListesi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 541);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "FrmUrunListesi";
+            this.Text = "FrmUrunListesi";
+            this.Load += new System.EventHandler(this.FrmUrunListesi_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtID.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtStok.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtSatisFiyat.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtAlisFiyat.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtMarka.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtUrunAd.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.SimpleButton BtnGuncelle;
+        private DevExpress.XtraEditors.SimpleButton BtnSil;
+        private DevExpress.XtraEditors.SimpleButton BtnKaydet;
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.TextEdit TxtStok;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.TextEdit TxtSatisFiyat;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.TextEdit TxtAlisFiyat;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.TextEdit TxtMarka;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit TxtUrunAd;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.SimpleButton BtnListele;
+        private DevExpress.XtraEditors.TextEdit TxtID;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+    }
+}
---
+++ TeknikServis/TeknikServis/Formlar/FrmUrunListesi.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeknikServis.Formlar
+{
+    public partial class FrmUrunListesi : Form
+    {
+        public FrmUrunListesi()
+        {
+            InitializeComponent();
+        }
+
+        DbTeknikServisEntities db = new DbTeknikServisEntities();
+        void metot1()
+        {
+            var degerler = from u in db.TBL_URUN
+                           select new
+                           {
+                               u.ID,
+                               u.AD,
+                               u.MARKA,
+                               KATEGORI = u.TBL_KATEGORI.AD,
+                               u.STOK,
+                               u.ALISFIYAT,
+                               u.SATISFIYAT
+                           };
+            gridControl1.DataSource = degerler.ToList();
+        }
+
+        private void FrmUrunListesi_Load(object sender, EventArgs e)
+        {
+            metot1();
+            lookUpEdit1.Properties.DataSource = db.TBL_KATEGORI.ToList();
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            TBL_URUN t = new TBL_URUN();
+            t.AD = TxtUrunAd.Text;
+            t.MARKA = TxtMarka.Text;
+            t.ALISFIYAT = decimal.Parse(TxtAlisFiyat.Text);
+            t.SATISFIYAT = decimal.Parse(TxtSatisFiyat.Text);
+            t.STOK = short.Parse(TxtStok.Text);
+            t.DURUM = false;
+            t.KATEGORI = byte.Parse(lookUpEdit1.EditValue.ToString());
+            db.TBL_URUN.Add(t);
+            db.SaveChanges();
+            MessageBox.Show("Ürün Başarıyla Kaydedildi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            labelControl7.Text = gridView1.GetFocusedRowCellValue("ID").ToString();
+            TxtUrunAd.Text = gridView1.GetFocusedRowCellValue("AD").ToString();
+            TxtMarka.Text = gridView1.GetFocusedRowCellValue("MARKA").ToString();
+            TxtAlisFiyat.Text = gridView1.GetFocusedRowCellValue("ALISFIYAT").ToString();
+            TxtSatisFiyat.Text = gridView1.GetFocusedRowCellValue("SATISFIYAT").ToString();
+            TxtStok.Text = gridView1.GetFocusedRowCellValue("STOK").ToString();
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(TxtID.Text);
+            var deger = db.TBL_URUN.Find(id);
+            db.TBL_URUN.Remove(deger);
+            db.SaveChanges();
+            MessageBox.Show("Ürün başarıyla silindi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(TxtID.Text);
+            var deger = db.TBL_URUN.Find(id);
+            deger.AD = TxtUrunAd.Text;
+            deger.STOK = short.Parse(TxtStok.Text);
+            deger.MARKA = TxtMarka.Text;
+            deger.ALISFIYAT = decimal.Parse(TxtAlisFiyat.Text);
+            deger.SATISFIYAT = decimal.Parse(TxtSatisFiyat.Text);
+            deger.KATEGORI = byte.Parse(lookUpEdit1.EditValue.ToString());
+            db.SaveChanges();
+            MessageBox.Show("Ürün başarıyla güncellendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            metot1();
+        }
+    }
+}
